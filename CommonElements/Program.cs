589b303be6acee2596306699d2ba1f12d18ad1f2
denisYordanov-eng <denisivov437@gmail.com>
2025-04-10 16:43:28 +0300

    Just made a task.

---
+++ CommonElements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            string[] arr2 = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                for (int j = 0; j < arr1.Length; j++)
+                {
+                    if (arr1[j] == arr2[i])
+                    {
+
+                        Console.Write(String.Join(" ", arr1[j]) + " ");
+                    }
+                }
+            }
+        }
+    }
+}
