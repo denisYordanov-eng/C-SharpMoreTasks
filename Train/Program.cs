dac445ef46ce5940e5f7f0a951be53fed6331eaf
denisYordanov-eng <denisivov437@gmail.com>
2025-04-10 16:18:24 +0300

    Just made a task.

---
+++ Train/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] passagers = new int[n];
+            long sum = 0L;
+            for (int i = 0; i < n; i++)
+            {
+                passagers[i] = int.Parse(Console.ReadLine());
+                sum += passagers[i];
+            }
+            Console.WriteLine(String.Join(" ", passagers));
+            Console.WriteLine(sum);
+        }
+    }
+}
