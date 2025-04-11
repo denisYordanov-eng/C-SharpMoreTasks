e009ad454178bc3ea509e5f1a1b95f7de2b7b588
denisYordanov-eng <denisivov437@gmail.com>
2025-04-11 09:57:50 +0300

    Made a task.

---
+++ Zig-ZagArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zig_ZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[n];
+
+           
+
+            for (int i = 0; i < n; i++)
+            {
+                
+                int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                if (i % 2 == 0)
+                {
+                    arr1[i] = input[0];
+                    arr2[i] = input[1];
+                }
+                else if(i % 2 != 0)
+                {
+                    arr2[i] = input[0];
+                    arr1[i] = input[1];
+                }
+            
+            }
+            Console.WriteLine(String.Join(" ", arr1));
+            Console.WriteLine(String.Join(" ", arr2));
+
+        }
+    }
+}
